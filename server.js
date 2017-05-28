dad94c6cc8e9223705b8b43ee9bf65c7c8e3523c
Pierre-François Pirlet <pfpirlet@hotmail.com>
2017-05-28 11:20:27 +0200

    software and language

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
+var response = {};
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+	var language = req.headers["accept-language"].split(",")[0];
+	var software = /\(([^)]+)\)/.exec(req.headers["user-agent"])[1];
+	response["language"] = language;
+	response["software"] = software;
+	res.send(response);
 })
 
 app.listen(port, function () {
